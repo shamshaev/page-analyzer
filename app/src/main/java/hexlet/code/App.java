b02ea7240e9e465308197295f8d3014016552ccc
shamshaev <an.shamshaev@gmail.com>
2024-06-12 01:34:53 +0300

    Implement environment variable PORT

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,11 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
     public static Javalin getApp() {
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
@@ -15,6 +20,6 @@ public class App {
 
     public static void main(String[] args) {
         var app = getApp();
-        app.start(7070);
+        app.start(getPort());
     }
 }
